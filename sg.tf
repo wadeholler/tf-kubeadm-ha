5c7015ebf79a050584eab9959e358149d8e4edff
Wade Holler <wade.holler@gmail.com>
2018-09-02 07:14:51 -0400

    update

--- sg.tf
+++ sg.tf
@@ -3,12 +3,6 @@ resource "aws_security_group" "kubeadm" {
   description = "Allow all kubeadm traffic"
   vpc_id      = "${var.vpc-id}"
 
-#  ingress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
   ingress {
     from_port   = 0
     to_port     = 0
