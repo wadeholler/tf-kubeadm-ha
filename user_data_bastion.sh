eeac2e117c558949c2e355a78fc793bc758b66c2
Wade Holler <wade.holler@gmail.com>
2018-09-07 22:16:55 -0400

    update

--- user_data_bastion.sh
+++ user_data_bastion.sh
@@ -5,7 +5,6 @@ if [ "$(. /etc/os-release; echo $NAME)" = "Ubuntu" ]; then
   sudo apt-get -y install python
   sudo apt-get -y install python-pip unzip
   sudo pip install ansible
-  rm -rf tf-kubeadm-ha || true
   git clone https://github.com/wadeholler/tf-kubeadm-ha.git
   wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip -O /tmp/output.zip
   unzip /tmp/output.zip
